8c28669ecd7bc12da01b18e3e30b29831145cfcf
Khoa Ho <khoa.ho@tuni.fi>
2026-01-02 17:45:40 +0200

    Add project files.

---
+++ Tutorial/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char myCharacter;
+	myCharacter = 'A';
+
+	int myInt;
+	myInt = 4;
+
+	cout << myCharacter << endl;
+	cout << myInt << endl;
+
+	myCharacter = 'n';
+	myInt = 100;
+
+	cout << myCharacter << endl;
+	cout << myInt << endl;
+
+	system("pause");
+}

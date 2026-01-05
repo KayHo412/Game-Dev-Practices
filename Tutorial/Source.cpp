3a22264b38e1a2aeb65b96aad52c86294dbc32fe
Khoa Ho <khoa.ho@tuni.fi>
2026-01-05 14:02:58 +0200

    Update : Learn about function in C++

--- Tutorial/Source.cpp
+++ Tutorial/Source.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 using namespace std;
 
+void welcome();
+char getYesNo();
+void printRespond(char responseToPrint);
+void AskUser();
+
 int main()
 {
-	char myCharacter;
-	myCharacter = 'A';
-
-	int myInt;
-	myInt = 4;
-
-	cout << myCharacter << endl;
-	cout << myInt << endl;
-
-	myCharacter = 'n';
-	myInt = 100;
-
-	cout << myCharacter << endl;
-	cout << myInt << endl;
+	// Asks user to enter Y or N and returns the response
+	AskUser();
 
 	system("pause");
+}
+
+void welcome()
+{
+	// Welcomes the user to the program
+	cout << "Welcome to C++ programming!" << endl;
+}
+
+char getYesNo()
+{
+	// Asks user to enter Y or N and returns the response
+	cout << "Please enter Y or N: " << endl;
+
+	char response;
+
+	// Get user input from keyboard
+	cin >> response; 
+
+	return response;
+}
+
+void printRespond(char responseToPrint)
+{
+	// Prints the response entered by the user
+	cout << "You entered: " << responseToPrint << endl;
+} 
+
+void AskUser()
+{
+	// Calls other functions to welcome user, get response, and print response
+	welcome();
+
+	char answer = getYesNo();
+
+	printRespond(answer);
 }
